4e833155a2cc997251a0f3014fb22042a0d70e47
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-16 15:18:16 +0200

    add script for add email in var "dataServer"

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
 const appEmailList = Vue.createApp({
     data() {
-
+        return {
+            dataServer: [],
+        }
     },
     methods: {
-
+        getEmailJSON(element){
+            for (let index = 0; index < 10; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(response => element[index] = response.data.response);
+            }
+        }
     },
 }).mount('#app')
