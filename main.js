ec61f6ec8bf61dad36424c7cb9d119bb35ed8041
antonionicolaci <antonionicolaci48@gmail.com>
2023-05-16 17:13:11 +0200

    add pre load array of emails

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const appEmailList = Vue.createApp({
     data() {
         return {
             dataServer: [],
+            dataPreLoad: [],
+            showList: false,
         }
     },
     methods: {
@@ -11,5 +13,13 @@ const appEmailList = Vue.createApp({
                     .then(response => element[index] = response.data.response);
             }
         }
+    },
+    computed: {
+        setDataPreLoad() {
+            for (let index = 0; index < 10; index++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                    .then(response => this.dataPreLoad[index] = response.data.response);
+            }
+        },
     },
 }).mount('#app')
